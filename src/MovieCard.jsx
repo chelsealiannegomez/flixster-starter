31c0abc72b68ae7ad93bcf4c6ed8fa2b12ef44aa
Chelsea Lianne Tan Gomez <chelseagomez@meta.com>
2025-06-09 16:46:16 -0700

    added image paths

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -4,7 +4,7 @@ const MovieCard = ( {prop} ) =>  {
     return (
         <div className="movie-card">
             {/* <img className="movie-img" src={prop.backdrop_path}/> */}
-            <img className="movie-img" src="/movie.png"/>
+            <img className="movie-img" src={"https://image.tmdb.org/t/p/w500/" + prop.backdrop_path}/>
             <h1>{prop.original_title}</h1>
             <p>Vote Average: {prop.vote_average}</p>
         </div>
